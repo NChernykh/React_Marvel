65f051c83bffb0aa61efa089652ab429d801231b
NadyaChernykh <nchernykh1@gmail.com>
2023-01-09 16:44:02 +0300

    Добавляет список героев

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,14 +11,29 @@ class MarvelService {
         return await res.json();
     }   
     
-    getAllCharacters = () => {
-        return this.getResource(
+    getAllCharacters = async () => {
+        const res = await this.getResource(
             `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        //Здесь формируется массив с объектами char из  _transformCharacter
+        return res.data.results.map(this._transformCharacter);    
     }
 
-    getCharacter = (id) => {
-        return this.getResource(
+    getCharacter = async (id) => {
+        const res = await this.getResource(
             `${this._apiBase}characters/${id}?${this._apiKey}`);
+        
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? char.description : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
